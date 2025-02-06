a4da5240876e2c080970c4fd9f9f2a7a6ab114a4
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-07 10:29:56 +1100

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,3 +1,6 @@
+import { cn } from '@/lib/utils';
+import { SquareDashedMousePointer } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react'
 
 interface LogoProps {
@@ -5,9 +8,13 @@ interface LogoProps {
   iconSize?: number;
 }
 
-function Logo({ fontSize = "2xl", iconSize = 20 }: LogoProps): React.ReactElement {
+function Logo({ fontSize = "text-2xl", iconSize = 20 }: LogoProps): React.ReactElement {
   return (
-    <div>Logo</div>
+    <Link href="/" className={cn("text-2xl font-extrabold flex item-center gap-2", fontSize)}>
+        <div className="rounded-xl bg-gradient-to-r from-emerald-50 to-emerald-600 p-2"> 
+            <SquareDashedMousePointer size={iconSize} className='stroke-white'/>
+        </div>
+    </Link>
   )
 }
 
