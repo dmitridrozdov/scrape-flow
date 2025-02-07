fbdbc41fbbaa5ecb0809c65692d83963716103b1
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-07 11:12:01 +1100

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -11,8 +11,16 @@ interface LogoProps {
 function Logo({ fontSize = "text-2xl", iconSize = 20 }: LogoProps): React.ReactElement {
   return (
     <Link href="/" className={cn("text-2xl font-extrabold flex item-center gap-2", fontSize)}>
-        <div className="rounded-xl bg-gradient-to-r from-emerald-50 to-emerald-600 p-2"> 
+        <div className="rounded-xl bg-gradient-to-r from-emerald-500 to-emerald-600 p-2"> 
             <SquareDashedMousePointer size={iconSize} className='stroke-white'/>
+        </div>
+        <div>
+            <span className="bg-gradient-to-r from-emerald-500 to-emerald-600 bg-clip-text text-transparent">
+                Flow
+            </span>
+            <span className='text-stone-700 dark:text-stone-300'>
+                Scrape
+            </span>
         </div>
     </Link>
   )
