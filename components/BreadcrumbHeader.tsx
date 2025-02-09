7d2fc93d9dfa0b5faa4708d288dbfee6777dcda5
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-09 15:20:05 +1100

    Update BreadcrumbHeader.tsx

--- components/BreadcrumbHeader.tsx
+++ components/BreadcrumbHeader.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
 const BreadcrumbHeader = () => {
+    const pathName = usePathname();
   return (
     <div>BreadcrumbHeader</div>
   )
