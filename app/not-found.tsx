b962abf8ee4e7bfabccbca2c7a14a070ab8f342e
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-10 16:12:32 +1100

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,6 +7,10 @@ const NotFoundPage = () => {
             <h1 className='text-4xl font-bold'>404</h1>
             <p className='text-2xl font-semibold'>Page not found</p>
             <p className='text-lg'>The page you are looking for does not exist.</p>
+            <div className='flex gap-2'>
+                <p className='text-lg'>Go back to</p>
+                <a className='text-lg font-semibold underline' href='/'>Home</a>
+            </div>
         </div>
     </div>
   )
