4f358723b9044c04c507e0c6fcf89512b19419b1
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-16 17:08:12 +1100

    Update page.tsx

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex flex-1 flex-col h-full'>
+        <div className='flex justify-between'>
+            <div className='flex flex-col'>
+                hiphop
+            </div>
+        </div>
+    </div>
   )
 }
 
