2ce1b6e53490891f7c4725513f16a00543f8bb8e
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-19 10:31:29 +1100

    Update CreateWorfklowDialog.tsx

--- app/(dashboard)/workflows/_components/CreateWorfklowDialog.tsx
+++ app/(dashboard)/workflows/_components/CreateWorfklowDialog.tsx
@@ -4,12 +4,12 @@ import React, { useState } from 'react'
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
 import { Button } from '@/components/ui/button'
 
-const CreateWorfklowDialog = () => {
+const CreateWorfklowDialog = ({ triggerText }: { triggerText?: string }) => {
     const [open, setOpen] = useState(false)
   return (
     <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
-            <Button onClick={() => setOpen(true)}>Create Workflow</Button>
+            <Button>{triggerText ?? "Create workflow"}</Button>
         </DialogTrigger>
         <DialogContent>
             <h1>Workflow</h1>
