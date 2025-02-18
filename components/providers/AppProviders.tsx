d37d62cf1df2871dfa83a1a4dc2bbb58ddce00e4
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-18 16:26:33 +1100

    Update AppProviders.tsx

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
-    return <ThemeProvider 
-                attribute='class' 
-                defaultTheme="system" 
-                enableSystem>
-                    {children}
-           </ThemeProvider>;
+    const [queryClient] = useState(() => new QueryClient())
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider 
+                    attribute='class' 
+                    defaultTheme="system" 
+                    enableSystem>
+                        {children}
+            </ThemeProvider>
+        </QueryClientProvider>
+    )
 }
