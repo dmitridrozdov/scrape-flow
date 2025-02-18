70220c6a308a66747b1cec4020999af38246b73f
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-19 10:56:31 +1100

    Create CustomDialogHeader.tsx

---
+++ components/CustomDialogHeader.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { DialogHeader, DialogTitle } from "@/components/ui/dialog"
+
+import React from 'react'
+
+interface Props {
+    title?: string
+    subTitle?: string
+    icon?: string
+    iconClassName?: string
+}
+const CustomDialogHeader = (props: Props) => {
+  return (
+    <div>CustomDialogHeader</div>
+  )
+}
+
+export default CustomDialogHeader
