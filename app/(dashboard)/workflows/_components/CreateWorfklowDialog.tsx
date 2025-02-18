62d63c25e16a98dc71f26921422edc7b9e929d0c
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-19 10:15:14 +1100

    Update CreateWorfklowDialog.tsx

--- app/(dashboard)/workflows/_components/CreateWorfklowDialog.tsx
+++ app/(dashboard)/workflows/_components/CreateWorfklowDialog.tsx
@@ -1,10 +1,20 @@
 "use client"
 
-import React from 'react'
+import React, { useState } from 'react'
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
+import { Button } from '@/components/ui/button'
 
 const CreateWorfklowDialog = () => {
+    const [open, setOpen] = useState(false)
   return (
-    <div>CreateWorfklowDialog</div>
+    <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+            <Button onClick={() => setOpen(true)}>Create Workflow</Button>
+        </DialogTrigger>
+        <DialogContent>
+            <h1>Workflow</h1>
+        </DialogContent>
+    </Dialog>
   )
 }
 
