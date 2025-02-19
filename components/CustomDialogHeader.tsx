627f3634a4c562392534f5fc53e2c763c0a6830c
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-20 06:43:48 +1100

    Update CustomDialogHeader.tsx

--- components/CustomDialogHeader.tsx
+++ components/CustomDialogHeader.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { cn } from "@/lib/utils"
 import { LucideIcon } from "lucide-react"
 
 import React from 'react'
@@ -14,11 +15,12 @@ interface Props {
     subTitleClassName?: string
 }
 const CustomDialogHeader = (props: Props) => {
+    const Icon = props.icon
   return (
     <DialogHeader className="py-6">
         <DialogTitle asChild>
             <div className="flex flex-col items-center gap-2 mb-2">
-
+                {Icon && <Icon size={30} className={cn("stroke-primary", props.iconClassName)} />}
             </div>
         </DialogTitle>
     </DialogHeader>
