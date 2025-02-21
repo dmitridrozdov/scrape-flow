506526abee3e76cc77e1a48df153752fc5d872d3
dmitridrozdov <dm.drozdov@gmail.com>
2025-02-21 15:06:58 +1100

    Update CreateWorfklowDialog.tsx

--- app/(dashboard)/workflows/_components/CreateWorfklowDialog.tsx
+++ app/(dashboard)/workflows/_components/CreateWorfklowDialog.tsx
@@ -5,9 +5,27 @@ import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
 import { Button } from '@/components/ui/button'
 import { Layers2Icon } from 'lucide-react'
 import CustomDialogHeader from '@/components/CustomDialogHeader'
+import { useForm } from 'react-hook-form'
+import { createWorkflowSchema } from '@/schema/workflow'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form'
 
 const CreateWorfklowDialog = ({ triggerText }: { triggerText?: string }) => {
     const [open, setOpen] = useState(false)
+    const form = useForm<z.infer<typeof createWorkflowSchema>>({
+        resolver: zodResolver(createWorkflowSchema),
+        defaultValues: {}
+    })
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
