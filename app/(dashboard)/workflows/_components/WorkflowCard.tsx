f7c61508dd259efc65ae6543be5bcf57e4a34d5d
dmitridrozdov <dm.drozdov@gmail.com>
2025-03-10 07:34:26 +1100

    Update WorkflowCard.tsx

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -1,16 +1,22 @@
 "use client"
 
 import { Card, CardContent } from '@/components/ui/card'
+import { WorflowStatus } from '@/types/workflow'
 import { Workflow } from '@prisma/client'
 import React from 'react'
 
 const WorkflowCard = ({workflow}: {workflow: Workflow}) => {
+    const isDraft = workflow.status === WorflowStatus.DRAFT
   return (
     <Card className='border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md dark: shadow-primary/30'>
         <CardContent>
-            <p>{workflow.name}</p>
-            <p>{workflow.description}</p>
-            <p>{workflow.status}</p>
+            <div className='flex items-center justify-between'>
+                <h3 className='font-bold'>{workflow.name}</h3>
+                <span className='text-sm text-muted-foreground'>
+                    {isDraft ? "Draft" : "Published"}
+                </span>
+            </div>
+            <p className='text-sm text-muted-foreground'>{workflow.description}</p>
         </CardContent>
     </Card>
   )
