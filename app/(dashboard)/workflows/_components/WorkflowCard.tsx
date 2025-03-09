b6c5bc2107b391693b13511ba9547680b03ae562
dmitridrozdov <dm.drozdov@gmail.com>
2025-03-10 07:41:09 +1100

    Update WorkflowCard.tsx

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -3,6 +3,7 @@
 import { Card, CardContent } from '@/components/ui/card'
 import { WorflowStatus } from '@/types/workflow'
 import { Workflow } from '@prisma/client'
+import { FileTextIcon, PlayIcon } from 'lucide-react'
 import React from 'react'
 
 const WorkflowCard = ({workflow}: {workflow: Workflow}) => {
@@ -11,10 +12,7 @@ const WorkflowCard = ({workflow}: {workflow: Workflow}) => {
     <Card className='border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md dark: shadow-primary/30'>
         <CardContent>
             <div className='flex items-center justify-between'>
-                <h3 className='font-bold'>{workflow.name}</h3>
-                <span className='text-sm text-muted-foreground'>
-                    {isDraft ? "Draft" : "Published"}
-                </span>
+                {isDraft ? <FileTextIcon className='h-5 w-5' /> : <PlayIcon className='h-5 w-5 text-white' />}
             </div>
             <p className='text-sm text-muted-foreground'>{workflow.description}</p>
         </CardContent>
