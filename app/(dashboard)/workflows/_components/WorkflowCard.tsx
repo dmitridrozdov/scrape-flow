465df0d55547af5497123b7eb662510d341862b1
dmitridrozdov <dm.drozdov@gmail.com>
2025-03-11 06:37:35 +1100

    Update WorkflowCard.tsx

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -18,19 +18,26 @@ const WorkflowCard = ({workflow}: {workflow: Workflow}) => {
   return (
     <Card className='border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md dark: shadow-primary/30'>
         <CardContent className='p-4 flex items-center justify-between h-[100px]'>
-            <div className={cn("w-10 h-10 rounded-full flex items-center justify-center", 
-                statusColors[workflow.status as WorflowStatus])}>
-                {isDraft ? <FileTextIcon className='h-5 w-5' /> : <PlayIcon className='h-5 w-5 text-white' />}
-            </div>
-            <div>
-                <h3 className='text-base font-bold text-muted-foreground flex items-center'>
-                    <Link 
-                        href={`/workflow/editor/${workflow.id}`}
-                        className='flex items-center hover:underline'
-                    >
-                        {workflow.name}
-                    </Link>
-                </h3>
+            <div className='flex items-center justify-end space-x-3'>
+                <div className={cn("w-10 h-10 rounded-full flex items-center justify-center", 
+                    statusColors[workflow.status as WorflowStatus])}>
+                    {isDraft ? <FileTextIcon className='h-5 w-5' /> : <PlayIcon className='h-5 w-5 text-white' />}
+                </div>
+                <div>
+                    <h3 className='text-base font-bold text-muted-foreground flex items-center'>
+                        <Link 
+                            href={`/workflow/editor/${workflow.id}`}
+                            className='flex items-center hover:underline'
+                        >
+                            {workflow.name}
+                        </Link>
+                        {isDraft && 
+                            <span className='ml-2 px-2 py-0.5 text-xs font-medium bg-yellow-100 text-yellow-800 rounded-full'>
+                                Draft
+                            </span>
+                        }
+                    </h3>
+                </div>
             </div>
         </CardContent>
     </Card>
