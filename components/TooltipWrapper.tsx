486ebfba40bcde61b98ad4facf51ed161b2e916c
dmitridrozdov <dm.drozdov@gmail.com>
2025-03-14 06:34:58 +1100

    Create TooltipWrapper.tsx

---
+++ components/TooltipWrapper.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+import React, { ReactNode } from 'react'
+
+interface Props {
+    children: ReactNode,
+    content: ReactNode,
+    side?: "top" | "right" | "bottom" | "left",
+
+}
+
+const TooltipWrapper = (props: Props) => {
+  return (
+    <TooltipProvider delayDuration={0}>
+        <Tooltip>
+            <TooltipTrigger asChild>{props.children}</TooltipTrigger>
+            <TooltipContent side={props.side}>{props.content}</TooltipContent> 
+        </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+export default TooltipWrapper
